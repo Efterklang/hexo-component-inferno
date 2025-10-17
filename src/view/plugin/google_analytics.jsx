f241b24b9e2eb546d271b3054e2b5fd5f75f2425
Efterklang <gaojiaxing2004@qq.com>
2025-10-17 21:29:32 +0800

    fix(google_analytics): update version to 3.1.4 and modify documentation feat(google_tag_manager): add Google Tag Manager component with caching support

--- src/view/plugin/google_analytics.jsx
+++ src/view/plugin/google_analytics.jsx
@@ -8,29 +8,27 @@ const { cacheComponent } = require('../../util/cache');
 /**
  * Google Analytics plugin JSX component.
  *
- * @see https://developers.google.com/tag-platform/tag-manager/web
+ * @see https://analytics.google.com/analytics/web
  * @example
- * <GoogleAnalytics trackingId="GTM-******" head={true} />
- * <GoogleAnalytics trackingId="GTM-******" head={false} />
+ * <GoogleAnalytics trackingId="******" />
  */
 class GoogleAnalytics extends Component {
   render() {
-    const { trackingId, head } = this.props;
+    const { trackingId } = this.props;
 
-    if (head) {
-      const js = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','${trackingId}');`;
-      return <script dangerouslySetInnerHTML={{ __html: js }}></script>;
-    }
+    const js = `window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', '${trackingId}');`;
 
     return (
-      <noscript
-        dangerouslySetInnerHTML={{
-          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${trackingId}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
-        }}></noscript>
+      <>
+        <script
+          async={true}></script>
+          src={`https://www.googletagmanager.com/gtag/js?id=${trackingId}`}
+        <script dangerouslySetInnerHTML={{ __html: js }}></script>
+      </>
     );
   }
 }
@@ -49,12 +47,11 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
  */
 GoogleAnalytics.Cacheable = cacheComponent(GoogleAnalytics, 'plugin.googleanalytics', (props) => {
   const { head, plugin } = props;
-  if (!plugin || !plugin.tracking_id) {
+  if (!head || !plugin.tracking_id) {
     return null;
   }
   return {
     trackingId: plugin.tracking_id,
-    head: Boolean(head),
   };
 });
 
